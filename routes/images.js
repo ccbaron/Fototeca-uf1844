1d4d3d8634761b96e35e7f993571cddf8e82598a
ccbaron <christian7baron@gmail.com>
2025-05-25 18:07:11 +0200

    Corrección

--- routes/images.js
+++ routes/images.js
@@ -5,17 +5,18 @@ const images = [];  // Simula una base de datos en memoria
 
 router.get('/', (req, res) => {
     res.render('home.ejs', { images });  // Renderiza la vista y le pasa las imágenes
-  });
+});
 
-  router.get('/new-image', (req, res) => {
+router.get('/new-image', (req, res) => {
     res.render('add-image.ejs', { message: undefined }); // Renderiza formulario vacío
-  });
+});
 
-  router.post('/new-image', (req, res) => {
+router.post('/new-image', (req, res) => {
     const { title, url } = req.body;
     images.push({ title, url });
     console.log('Imagen añadida:', { title, url });
     res.render('add-image.ejs', { message: 'La imagen se ha añadido correctamente' });
-  });
-  
-  
+});
+
+module.exports = router;
+
